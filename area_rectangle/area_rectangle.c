d0ebd86f29599aab8ac44ccff73b8d51a3a9d207
Matthew Wright <matthewbwright@gmail.com>
2019-03-29 13:55:42 -0700

    adds code for area of a rectangle

--- area_rectangle/area_rectangle.c
+++ area_rectangle/area_rectangle.c
@@ -2,4 +2,13 @@
 
 int main()
 {
+  double rectangle_width = 5;
+  double rectangle_height = 20;
+  double rectangle_perimeter = (rectangle_width + rectangle_height) * 2;
+  double rectangle_area = rectangle_width * rectangle_height;
+
+  printf("The perimeter of the rectangle is %f.\n", rectangle_perimeter);
+  printf("The area of the rectangle is %f.\n", rectangle_area);
+
+  return 0;
 }
