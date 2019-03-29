413ed226a38e44256a496cca11571f134b858e48
Matthew Wright <matthewbwright@gmail.com>
2019-03-29 14:05:30 -0700

    changes decimals displayed

--- area_rectangle/area_rectangle.c
+++ area_rectangle/area_rectangle.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-  double rectangle_width = 5;
-  double rectangle_height = 20;
-  double rectangle_perimeter = (rectangle_width + rectangle_height) * 2;
+  double rectangle_width = 5.0;
+  double rectangle_height = 20.0;
+  double rectangle_perimeter = (rectangle_width + rectangle_height) * 2.0;
   double rectangle_area = rectangle_width * rectangle_height;
 
-  printf("The perimeter of the rectangle is %f.\n", rectangle_perimeter);
-  printf("The area of the rectangle is %f.\n", rectangle_area);
+  printf("The perimeter of the rectangle is %.2f.\n", rectangle_perimeter);
+  printf("The area of the rectangle is %.2f.\n", rectangle_area);
 
   return 0;
 }
